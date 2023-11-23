97fd780696ed1e94e7b470aa639fe0467b53b39d
bandillamylene <bandillamylene.dev@gmail.com>
2023-11-23 17:01:58 +0800

    Mission update2

---
+++ aboutus.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+      setTimeout(() => {
+        card.style.bottom = '0';
+      }, index * 200); // Adjust the delay between each card's animation
+    });
+  });
+  
