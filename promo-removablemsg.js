7ef0a0431738360345804bf191d2d83e56751813
bandillamylene <bandillamylene.dev@gmail.com>
2023-12-15 00:07:59 +0800

    function modified-removablemsg

---
+++ promo-removablemsg.js
@@ -0,0 +1,4 @@
+document.querySelector('.clickMe').addEventListener('click', function() {
+    // Hide the element with the class 'click-cards'
+    document.querySelector('.click-cards').style.display = 'none';
+});
--- removablemessage.js
+++ removablemessage.js
@@ -1,8 +1,4 @@
-// JavaScript to handle closing the banner in the index and cards message in the promo page when the '.clickMe' element is clicked
 document.querySelector('.clickMe').addEventListener('click', function() {
-    // Hide the element with the class 'click-cards'
-    document.querySelector('.click-cards').style.display = 'none';
-
     // Hide the element with the class 'promo-banner'
     document.querySelector('.promo-banner').style.display = 'none';
 });
