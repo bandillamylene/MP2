5b91ed76e8650653e84b22814ad18c1c21d8cacb
bandillamylene <bandillamylene.dev@gmail.com>
2023-12-17 11:40:47 +0800

    userDashboard update

--- bookMark.js
+++ bookMark.js
@@ -1,39 +1,43 @@
 // Function to display saved items with product--card content
 function displaySavedItems() {
-  const savedItems = JSON.parse(localStorage.getItem('savedItems')) || [];
-  const savedItemsContainer = document.getElementById('savedItems');
-
-  savedItemsContainer.innerHTML = '';
-
-  if (savedItems.length === 0) {
+    const savedItems = JSON.parse(localStorage.getItem('savedItems')) || [];
+    const savedItemsContainer = document.getElementById('savedItems');
+  
+    savedItemsContainer.innerHTML = '';
+  
+    if (savedItems.length === 0) {
       savedItemsContainer.innerHTML = '<p>No saved items yet.</p>';
-  } else {
+    } else {
       let html = '';
       const colClasses = {
-          xl: 'col-xl-4 d-flex justify-content-center',
-          lg: 'col-lg-4 d-flex justify-content-center',
-          md: 'col-md-6 d-flex justify-content-center',
-          sm: 'col-12 d-flex justify-content-center',
+        lg: 'col-lg-4 col-xl-4 d-flex justify-content-center', // 3 cards in a row for LG screens
+        md: 'col-md-6 d-flex justify-content-center', // 2 cards in a row for MD screens
+        sm: 'col-12 d-flex justify-content-center', // 1 card in a row for SM screens
+        xs: 'col-12 d-flex justify-content-center' // 1 card in a row for XS screens
       };
-
+  
       savedItems.forEach((item, index) => {
-          const breakpoint = window.innerWidth >= 1200 ? 'xl' : (window.innerWidth >= 992 ? 'lg' : (window.innerWidth >= 768 ? 'md' : 'sm'));
-          const colClass = colClasses[breakpoint];
-
-          html += `
-              <div class="${colClass} mb-4">
-                  <div class="smaller-card">${item}</div>
-              </div>`;
-
-          html += '<div class="w-100 d-block d-sm-none"></div>'; // Add a new row for SM and XS screens
+        const breakpoint = window.innerWidth >= 1200 ? 'lg' : window.innerWidth >= 992 ? 'md' : window.innerWidth >= 768 ? 'sm' : 'xs';
+        const colClass = colClasses[breakpoint];
+  
+        html += `
+          <div class="${colClass} mb-4">
+            <div class="smaller-card">${item}</div>
+          </div>`;
+  
+        // For SM and XS screens, add a line break after each card
+        if (breakpoint === 'sm' || breakpoint === 'xs') {
+          html += '<div class="w-100"></div>';
+        }
       });
-
+  
       savedItemsContainer.innerHTML = html;
+    }
   }
-}
-
-// Initial call to display saved items when the page loads
-displaySavedItems();
-
-// Event listener to update card layout on window resize
-window.addEventListener('resize', displaySavedItems);
+  
+  // Initial call to display saved items when the page loads
+  displaySavedItems();
+  
+  // Event listener to update card layout on window resize
+  window.addEventListener('resize', displaySavedItems);
+  
--- userDashboard.js
+++ userDashboard.js
@@ -1,177 +1,52 @@
-// Function to check login status
-function checkLoginStatus() {
-    const loggedIn = sessionStorage.getItem("loggedIn");
-    return loggedIn === "true";
-}
-
-// Function to enable/disable form fields for editing
-function enableFormEditing(enable) {
-    var inputs = document.querySelectorAll("#personalInfoForm input");
-    inputs.forEach(function (input) {
-        input.disabled = !enable;
-    });
-
-    var saveBtn = document.getElementById("saveBtn");
-    saveBtn.style.display = enable ? "block" : "none";
-}
-
-// Function to save edited data to session storage
-function saveEditedData() {
-    var userData = {};
-    var isValid = true;
-    var errorMessages = document.querySelectorAll(".form__input--error-message");
-
-    errorMessages.forEach(function (errorMessage) {
-        errorMessage.style.display = "none";
-
-        var inputField = errorMessage.nextElementSibling; // Target input fields
-        userData[inputField.id] = inputField.value.trim();
-
-        if (inputField.value.trim() === "") {
-            errorMessage.style.display = "block";
-            isValid = false;
-        }
-    });
-
-    if (isValid) {
-        sessionStorage.setItem("userData", JSON.stringify(userData));
-        console.log("Data saved to session storage:", userData);
-        enableFormEditing(false); // Disabling editing mode upon successful save
-        document.getElementById("saveBtn").style.display = "none"; // Hide the save button
-    } else {
-        console.log("Cannot save. Please fill in all required fields.");
-        // Displaying error messages for required fields
-        // The error messages are already provided in the HTML
-    }
-}
-
-// Function to handle applying for a loan
-function applyForLoan() {
-    window.location.href = "loan_application.html";
-}
-
-// Function to handle logout
-function logoutUser() {
-    sessionStorage.removeItem("userData");
-    sessionStorage.setItem("loggedIn", "false"); // Update login status
-    window.location.href = "login.html";
-}
-
-// Function to display specific dashboard section
-function showDashboardSection(sectionId) {
-    var dashboardSections = document.querySelectorAll(".col-lg-8");
-
-    dashboardSections.forEach(function (section) {
-        section.style.display = "none";
-    });
-
-    var selectedSection = document.getElementById(sectionId);
-    if (selectedSection) {
-        selectedSection.style.display = "block";
-    }
-}
-
-// Function to check login status and handle navigation
-window.addEventListener("load", function () {
-    if (!checkLoginStatus()) {
-        history.pushState(null, null, location.href);
-        window.onpopstate = function () {
-            history.go(1);
-        };
-    }
-});
-
-// Event listeners for dashboard links and logout
 document.addEventListener("DOMContentLoaded", function () {
-    if (!checkLoginStatus()) {
-        window.location.href = "login.html";
+    // Function to hide all dashboard sections except the given section
+    function hideAllExcept(sectionId) {
+        var dashboardSections = document.querySelectorAll(".col");
+        dashboardSections.forEach(function (section) {
+            if (section.id !== sectionId) {
+                section.style.display = "none";
+            } else {
+                section.style.display = "block";
+            }
+        });
     }
 
+    // Initially show the Settings section
+    hideAllExcept("mysettings");
+
+    // Event listeners for dashboard links
     var savedItemsLink = document.getElementById("savedItemsLink");
     if (savedItemsLink) {
         savedItemsLink.addEventListener("click", function (event) {
-            event.preventDefault();
-            showDashboardSection("mysaveditems");
+            hideAllExcept("mysaveditems");
         });
     }
 
-    var savedItemsLink = document.getElementById("settingsLink");
-    if (savedItemsLink) {
-        savedItemsLink.addEventListener("click", function (event) {
-            event.preventDefault();
-            showDashboardSection("mysettings");
+    var settingsLink = document.getElementById("settingsLink");
+    if (settingsLink) {
+        settingsLink.addEventListener("click", function (event) {
+            hideAllExcept("mysettings");
         });
     }
 
-    var savedItemsLink = document.getElementById("loanTrackerLink");
-    if (savedItemsLink) {
-        savedItemsLink.addEventListener("click", function (event) {
-            event.preventDefault();
-            showDashboardSection("myloans");
+    var loanTrackerLink = document.getElementById("loanTrackerLink");
+    if (loanTrackerLink) {
+        loanTrackerLink.addEventListener("click", function (event) {
+            hideAllExcept("myloans");
         });
     }
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+    var dashboardItems = document.getElementById("dashboardItems");
+    var dashboardToggle = document.getElementById("dashboardToggle");
 
-    var logoutBtns = document.querySelectorAll("#logoutBtn");
-    logoutBtns.forEach(function (logoutBtn) {
-        logoutBtn.addEventListener("click", function () {
-            logoutUser();
-        });
+    dashboardToggle.addEventListener("click", function () {
+        if (dashboardItems.style.display === "none") {
+            dashboardItems.style.display = "block";
+        } else {
+            dashboardItems.style.display = "none";
+        }
     });
-
-    // Event listener for the login form
-    var loginForm = document.getElementById("loginForm");
-    if (loginForm) {
-        loginForm.addEventListener("submit", function (e) {
-            e.preventDefault();
-            var emailInput = document.getElementById("emailInput").value;
-            var passwordInput = document.getElementById("passwordInput").value;
-            loginUser(emailInput, passwordInput);
-        });
-    }
-
-     // Event listener for the "Save" button
-     var saveBtn = document.getElementById("saveBtn");
-     if (saveBtn) {
-         saveBtn.addEventListener("click", function () {
-             saveEditedData();
-         });
-     }
-
-    // Event listener for the "Edit" button
-    var editBtn = document.getElementById("editBtn");
-    if (editBtn) {
-        editBtn.addEventListener("click", function () {
-            enableFormEditing(true);
-        });
-    }
-
-    // Event listener for the "Apply for a Loan" button
-    var applyLoanBtn = document.getElementById("applyLoanBtn");
-    if (applyLoanBtn) {
-        applyLoanBtn.addEventListener("click", function () {
-            applyForLoan();
-        });
-    }
-
-    // Retrieve saved data when the DOM is loaded
-    var savedData = sessionStorage.getItem("userData");
-    if (savedData) {
-        var userData = JSON.parse(savedData);
-
-        Object.keys(userData).forEach(function (key) {
-            document.getElementById(key).value = userData[key];
-        });
-    }
 });
 
-
-// user_dashboard.js preventing to go back when logged in
-document.addEventListener("DOMContentLoaded", () => {
-    const loggedIn = sessionStorage.getItem("loggedIn");
-
-    if (loggedIn !== "true") {
-        window.location.href = "login.html";
-    }
-    
-});
